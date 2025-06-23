875a207c55ba0b3d800664520c75409e89f2e6d1
ibuki <chenyao.pan33@gmail.com>
2025-06-23 13:06:01 +0200

    fix coding format and add prettier format check

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,57 +1,55 @@
 const Header = (props) => {
-  return(
+  return (
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
-}
+  );
+};
 
 const Part = (props) => {
-  return(
+  return (
     <div>
       <p>
         {props.part} {props.exercises}
       </p>
     </div>
-  )
-}
+  );
+};
 
 const Content = (props) => {
-  const items = props.items
-  return(
+  const items = props.items;
+  return (
     <div>
-      {
-        items.map((item) => (
-          <Part key={item.part} part={item.part} exercises={item.exercises} />
-        ))
-      }
+      {items.map((item) => (
+        <Part key={item.part} part={item.part} exercises={item.exercises} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Total = (props) => {
-  const items = props.items
-  const total = items.reduce((sum, item) => sum + item.exercises, 0)
+  const items = props.items;
+  const total = items.reduce((sum, item) => sum + item.exercises, 0);
   return (
     <div>
       <p>Number of exercises {total}</p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
   const part_lst = [
     { part: part1, exercises: exercises1 },
     { part: part2, exercises: exercises2 },
-    { part: part3, exercises: exercises3 }
-  ]
+    { part: part3, exercises: exercises3 },
+  ];
 
   return (
     <div>
@@ -59,7 +57,7 @@ const App = () => {
       <Content items={part_lst} />
       <Total items={part_lst} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
