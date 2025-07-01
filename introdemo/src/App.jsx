37a2e5642ae0c59c64d95d0c801146baf3509122
Ibuki333 <chenyao.pan33@gmail.com>
2025-07-01 18:02:27 +0200

    apply destructuring

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,23 +1,22 @@
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </div>
   );
 };
 
-const Part = (props) => {
+const Part = ({ key, part, exercises }) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {part} {exercises}
       </p>
     </div>
   );
 };
 
-const Content = (props) => {
-  const parts = props.parts;
+const Content = ({ parts }) => {
   return (
     <div>
       {parts.map((part) => (
@@ -27,8 +26,7 @@ const Content = (props) => {
   );
 };
 
-const Total = (props) => {
-  const parts = props.parts;
+const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <div>
